afc2f39dc8147977252111df23896f77ff558ff5
Nicolas Berger <nicoberger@gmail.com>
2014-08-30 16:07:42 -0300

    Mostrando las nubes de todos los medios

--- front/js/quesedijo.js
+++ front/js/quesedijo.js
@@ -13,15 +13,6 @@ var data =  {
       ]
     },
     {
-      from: "2014-07-08",
-      to: "2014-07-14",
-      words: [
-      {word: "fondos buitre", freq: 12},
-      {word: "inflacion", freq: 7},
-      {word: "dolar", freq: 9}
-      ]
-    }  ,
-    {
       from: "2014-07-15",
       to: "2014-07-21",
       words: [
@@ -71,33 +62,31 @@ var data =  {
       {word: "inflacion", freq: 7},
       {word: "dolar", freq: 9}
       ]
-    }  ,
-    {
-      from: "2014-07-22",
-      to: "2014-07-28",
-      words: [
-      {word: "fondos buitre", freq: 17},
-      {word: "inflacion", freq: 2},
-      {word: "messi", freq: 18},
-      {word: "dolar", freq: 8}
-      ]
-    }
+    },
     ]
   }
   ]
 };
 
-Medios = {
-
-}
-
 $(function() {
   bindSlider();
-
-  drawTagClouds(data);
 });
 
-function drawTagClouds(data) {
+function getPeriods(data) {
+  var periods = data.medios.map(function(medio) {
+    return medio.periodos.map(function(periodo) {
+      return {from: periodo.from, to: periodo.to};
+    });
+  });
+
+  return _.chain(periods)
+    .flatten()
+    .uniq(false, function(period) { return period.from; })
+    .sortBy("from")
+    .value();
+}
+
+function drawTagClouds(data, period) {
   var container = $(".medios");
   container.empty();
   console.log("1");
@@ -107,33 +96,51 @@ function drawTagClouds(data) {
     var $medio = $("<li class='medio'><h2>" + medio.name + "</h2><div class='cloud' /></li>");
     container.append($medio);
 
-    var words = medio.periodos[0].words.map(function(w) {
-      return {text: w.word, size: w.freq};
+    var medioPeriod = _.find(medio.periodos, function(p) {
+      return p.from === period.from;
     });
 
-    d3.layout.cloud().size([300, 150])
-    .words(words)
-    .padding(5)
-    .rotate(function() { return ~~(Math.random() * 2) * 90; })
-    .font("Impact")
-    .fontSize(function(d) { return d.size * 2; })
-    .on("end", function(words) { return draw(words, $medio.find(".cloud")[0]); })
-    .start();
+    if (medioPeriod) {
+      var words = medioPeriod.words.map(function(w) {
+        return {text: w.word, size: w.freq};
+      });
+
+      d3.layout.cloud().size([300, 150])
+      .words(words)
+      .padding(5)
+      .rotate(function() { return ~~(Math.random() * 2) * 90; })
+      .font("Impact")
+      .fontSize(function(d) { return d.size * 2; })
+      .on("end", function(words) { return draw(words, $medio.find(".cloud")[0]); })
+      .start();
+    } else {
+      $medio.append("<p>No hay datos para estas fechas en " + medio.name + "</p>");
+    }
   }
   );
+}
+
+function periodLabel(period) {
+  return "Desde " + period.from + " hasta " + period.to;
 }
 
 function bindSlider() {
   $( "#dates-slider" ).slider({
     range: "min",
-    value: 37,
+    value: 0,
     min: 0,
-    max: 3,
+    max: getPeriods(data).length - 1,
     slide: function( event, ui ) {
-      $( "#selected-date" ).text( ui.value );
+      var period = getPeriods(data)[ui.value];
+      $( "#selected-date" ).text( periodLabel(period) );
+
+      drawTagClouds(data, period);
     }
   });
-  $( "#selected-date" ).text( $( "#dates-slider" ).slider( "value" ) );
+
+  var period = getPeriods(data)[0];
+  $( "#selected-date" ).text( periodLabel(period) );
+  drawTagClouds(data, period);
 }
 
 var fill = d3.scale.category20();
